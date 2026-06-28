23e9757a42057c2df8500ee10682fcabeac9a6f1
Aman Verma <43925820+aman11verma@users.noreply.github.com>
2026-06-29 00:52:38 +0530

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length())
+        return false;
+        std::unordered_map<char, int> orig_chars;
+        for(int i=0;i<s.length();i++){
+            orig_chars[s[i]]++;
+        }
+        for(int i=0;i<t.length();i++){
+            if(orig_chars[t[i]]==0)
+            return false;
+            orig_chars[t[i]]--;
+        }
+    for(auto it = orig_chars.begin(); it!=orig_chars.end();it++){
+        if(it->second!=0)
+        return false;
+    }
+        return true;
+    }
+};
